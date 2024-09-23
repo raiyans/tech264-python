15632bab5e0e176b52ce8427013e470b2e4817fd
Raiyan Shaheen <rishaheen@spartaglobal.com>
2024-09-23 17:35:58 +0100

    [feat] added operators script running and [docs] explained the task

---
+++ operators/operators.md
@@ -0,0 +1,8 @@
+## Explain: What is the difference between an operator and operand?
+Operator: Symbol that performs the action.
+Operand: The values or variables the operator acts on.
+```python
+x + y  # '+' is the operator (addition)
+x + y  # 'x' and 'y' are operands
+```
+
---
+++ operators/operators_examples.py
@@ -0,0 +1,45 @@
+# Addition
+x = 5
+y = 1
+addition = x + y
+print("Addition:", addition)
+
+# Subtraction
+subtraction = x - y
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = x * y
+print("Multiplication:", multiplication)
+
+# Division
+division = x / y
+print("Division:", division)
+
+# Remainder (Modulus)
+remainder = x % y
+print("Remainder:", remainder)
+
+# Greater than
+is_greater = x > y
+print("Is x greater than y?", is_greater)
+
+# Less than
+is_less = x < y
+print("Is x less than y?", is_less)
+
+# Check if equal
+is_equal = x == y
+print("Is x equal to y?", is_equal)
+
+# Check if not equal
+is_not_equal = x != y
+print("Is x not equal to y?", is_not_equal)
+
+# Greater than or equal to
+is_greater_or_equal = x >= y
+print("Is x greater than or equal to y?", is_greater_or_equal)
+
+# Less than or equal to
+is_less_or_equal = x <= y
+print("Is x less than or equal to y?", is_less_or_equal)
