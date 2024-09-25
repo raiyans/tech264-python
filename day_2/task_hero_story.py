1f3f8aa670ba7218123d4b40f228abe189840eb8
Raiyan Shaheen <rishaheen@spartaglobal.com>
2024-09-25 14:44:50 +0100

    [feat] missing dob code and hero task started

---
+++ day_2/task_hero_story.py
@@ -0,0 +1,30 @@
+"""
+Write a Python script to write a 'Hero story'
+Purpose
+
+Practice what you've learnt about Python dictionaries
+The task
+
+First part
+
+Define a dictionary called story1. It should have the following keys:
+'start', 'middle' and 'end'
+Print the entire dictionary
+Print the type of your dictionary
+Print only the keys
+Print only the values
+Print the individual values using the keys (individually, lots of print commands)
+Now, let's add a new key:value pair:
+'hero': yourSuperHero
+Hints:
+
+THE MORE CREATIVE THE BETTER
+If time
+
+Improve the program. For example, can you make it a "Choose your own adventure" story?
+"""
+
+story1 ={"start":"there was a house on fire","middle","end"}
+print(story1)
+print(type(story1)) #prints a set type
+print(story1) #prints a set type
